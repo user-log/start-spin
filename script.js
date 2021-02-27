afdcd8177c47a13702bf8f9498d1bf6caf2aa29b
user-log <76106375+user-log@users.noreply.github.com>
2021-02-27 20:10:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+jQuery(function($){
+  $(document).ajaxSend(function() {
+    $("#overlay").fadeIn(300);　
+  });
+    
+  $('#button').click(function(){
+    $.ajax({
+      type: 'GET',
+      success: function(data){
+        console.log(data);
+      }
+    }).done(function() {
+      setTimeout(function(){
+        $("#overlay").fadeOut(300);
+      },500);
+    });
+  }); 
+  setTimeout(function() {   //calls click event after a certain time
+    $(location).attr('href','https://spin2win-lg-ar.herokuapp.com')
+ }, 3000);
+
+});

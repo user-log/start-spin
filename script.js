09fd53d24e5938c53955afcd3f64a973662bdeda
user-log <76106375+user-log@users.noreply.github.com>
2021-03-25 00:03:36 +0200

    Update script.js

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ jQuery(function($){
     });
   }); 
   setTimeout(function() {   //calls click event after a certain time
-    $(location).attr('href','https://bit.ly/2XkwfOl')
- }, 3000);
+    $(location).attr('href','https://spin2win-lg.herokuapp.com/')
+ }, 2500);
 
 });
